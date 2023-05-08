7506217cc55f2e8608c02d703c271518d181747c
2707066 <80663695+Sergey270@users.noreply.github.com>
2023-05-08 16:30:41 +0600

    Wakelock.disable

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    Wakelock.disable();
+    Wakelock.enable();
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: MainPage(),
